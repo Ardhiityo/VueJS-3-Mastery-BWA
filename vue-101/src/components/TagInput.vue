f69d09bde3861dcdbf4443e78ce717aa14e29617
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-19 21:54:24 +0700

    Memberikan style pada komponen

--- vue-101/src/components/TagInput.vue
+++ vue-101/src/components/TagInput.vue
@@ -9,7 +9,7 @@ export default {
         value(newVal, olVal) {
             if (newVal.includes(',')) {
                 this.value = newVal.slice(0, -1);
-                this.addTags();
+                this.addNewTag();
             }
         }
     },
@@ -19,15 +19,15 @@ export default {
         }
     },
     methods: {
-        addTags: function () {
+        addNewTag: function () {
             if (!this.isTagExists && this.value.length) {
                 this.tags.push(this.value);
             }
         },
-        removeTags: function (index) {
+        removeTag: function (index) {
             this.tags.splice(index, 1);
         },
-        removeLastTags: function () {
+        removeLastTag: function () {
             if (this.value.length === 0) {
                 this.tags.pop()
             }
@@ -39,7 +39,7 @@ export default {
 <template>
     <section>
         <div v-for="(tag, index) in tags" :key="index">
-            {{ tag }} <span @click="removeTags(index)">&times;</span>
+            {{ tag }} <span @click="removeTag(index)">&times;</span>
         </div>
 
         <!-- Digunakan untuk menampilkan teks apa-adanya, apabila memiliki whitespace maka ditampilkan -->
@@ -62,14 +62,66 @@ export default {
 
         <!-- Cara baru : 2 flow data binding -->
         <!-- Trim untuk menghapus whitespace -->
-        <input type="text" v-model.trim="value" @keydown.backspace="removeLastTags" @keydown.enter="addTags"
-            @keydown.tab.prevent="addTags" :class="{ tagExists: isTagExists }">
+        <!-- <input type="text" v-model.trim="value" @keydown.backspace="removeLastTags" @keydown.enter="addTags"
+            @keydown.tab.prevent="addTags" :class="{ 'tag-exists': isTagExists }"> -->
+
+        <div class="tags-input-wrapper">
+            <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+                {{ tag }}
+                <a class="remove-tag" @click.prevent="removeTag(index)" href="#">&times;</a>
+            </span>
+            <input class="tag-input" type="text" v-model.trim="value" @keydown.enter="addNewTag"
+                @keydown.delete="removeLastTag" @keydown.tab.prevent="addNewTag"
+                :class="{ 'tag-exists': isTagExists }" />
+        </div>
+
     </section>
 </template>
 
 <style scoped>
-.tagExists {
+.tag-input.tag-exists {
     color: red;
-    text-decoration: line-through
+    text-decoration: line-through;
+}
+
+.tags-input-wrapper {
+    background: #fff;
+    padding: 0.5em;
+    border: 1px solid #dbdbdb;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    min-height: 36px;
+    box-sizing: border-box;
+}
+
+.tag-item {
+    color: #212529;
+    background-color: #eee;
+    margin-right: 0.3em;
+    padding: 0.25em 0.4em;
+    font-size: 75%;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-right: 1.25em;
+    padding-left: 0.6em;
+}
+
+.tag-input {
+    color: #495057;
+    flex: 1;
+    background: transparent;
+    border: none;
+}
+
+.tag-input:focus {
+    outline: none;
+}
+
+a.remove-tag {
+    text-decoration: none;
 }
 </style>
