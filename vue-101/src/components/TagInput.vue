98eedb8513e6857831a5621ac968a0c78a6329a5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-19 19:13:44 +0700

    Mengenal Sintaks Vue js

--- vue-101/src/components/TagInput.vue
+++ vue-101/src/components/TagInput.vue
@@ -1,9 +1,18 @@
 <script>
 export default {
-    name: 'TagInput'
+    name: 'TagInput',
+    data: () => ({
+        value: 'tes',
+        tags: ['vue', 'react', 'svelte']
+    })
 }
 </script>
 
 <template>
-    <input type="text" name="" id="">
+    <section>
+        <div v-for="(tag, index) in tags" :key="index">
+            {{ tag }}
+        </div>
+        <input type="text" name="" id="" :value="value">
+    </section>
 </template>
