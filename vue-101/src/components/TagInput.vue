f84356b6f0688cabfbf73f51573e80de5b77d496
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-19 19:58:04 +0700

    Teknik Two Way Binding dengan V-Model

--- vue-101/src/components/TagInput.vue
+++ vue-101/src/components/TagInput.vue
@@ -14,6 +14,9 @@ export default {
             {{ tag }}
         </div>
 
+        <!-- Digunakan untuk menampilkan teks apa-adanya, apabila memiliki whitespace maka ditampilkan -->
+        <pre>{{ value }}</pre>
+
         <!-- Tab.prevent untuk mencegah fokus ke element lain -->
 
         <!-- Event 1 -->
@@ -21,7 +24,17 @@ export default {
             v-on:keydown.tab.prevent="tags.push($event.target.value)"> -->
 
         <!-- Event 2 -->
-        <input type="text" :value="value" @keydown.enter="tags.push($event.target.value)"
+        <!-- <input type="text" :value="value" @keydown.enter="tags.push($event.target.value)"
+            @keydown.tab.prevent="tags.push($event.target.value)"> -->
+
+        <!-- Cara lama : 2 flow data binding  -->
+        <!-- @input digunakan untuk mengambil value terbaru apabila terjadi perubahan -->
+        <!-- <input type="text" :value="value" @input="value = $event.target.value"
+            @keydown.enter="tags.push($event.target.value)" @keydown.tab.prevent="tags.push($event.target.value)"> -->
+
+        <!-- Cara baru : 2 flow data binding -->
+        <!-- Trim untuk menghapus whitespace -->
+        <input type="text" v-model.trim="value" @keydown.enter="tags.push($event.target.value)"
             @keydown.tab.prevent="tags.push($event.target.value)">
     </section>
 </template>
