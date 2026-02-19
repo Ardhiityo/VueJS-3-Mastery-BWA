6e80c67fe7df62746cbb7dd6bfb5c9d59d908a58
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-19 19:39:07 +0700

    Penanganan Event

--- vue-101/src/components/TagInput.vue
+++ vue-101/src/components/TagInput.vue
@@ -13,6 +13,15 @@ export default {
         <div v-for="(tag, index) in tags" :key="index">
             {{ tag }}
         </div>
-        <input type="text" name="" id="" :value="value">
+
+        <!-- Tab.prevent untuk mencegah fokus ke element lain -->
+
+        <!-- Event 1 -->
+        <!-- <input type="text" :value="value" v-on:keydown.enter="tags.push($event.target.value)"
+            v-on:keydown.tab.prevent="tags.push($event.target.value)"> -->
+
+        <!-- Event 2 -->
+        <input type="text" :value="value" @keydown.enter="tags.push($event.target.value)"
+            @keydown.tab.prevent="tags.push($event.target.value)">
     </section>
 </template>
