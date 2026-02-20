6d564ce0ee9b14f32e31c4ecaba8851d2baa7752
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-20 14:00:13 +0700

    Props Statis dan Dinamis

---
+++ bekerja dengan komponen/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+import Alerts from "./components/Alerts.vue"
+import LoginForm from "./components/auth/LoginForm.vue"
+import BaseInput from "./components/BaseInput.vue"
+import BaseButton from "./components/BaseButton.vue"
+import IconCheck from "./components/icons/IconCheck.vue"
+import IconExclamation from "./components/icons/IconExclamation.vue"
+import Modal from "./components/Modal.vue"
+import TodoList from "./components/TodoList.vue"
+import Person from "./components/Person.vue"
+import Alert from "./components/Alert.vue"
+
+export default {
+  components: {
+    Person,
+    Alerts,
+    Alert,
+    LoginForm,
+    BaseInput,
+    BaseButton,
+    Modal,
+    IconCheck,
+    IconExclamation,
+    TodoList
+  },
+  data: () => ({
+    items: 3,
+    show: true,
+    alert: {
+      message: "Message",
+      types: ["success", "warning", "danger"],
+    },
+    email: '',
+    password: '',
+    message: "Alert message"
+  }),
+  methods: {
+    handleSubmit(email, password) {
+      alert(email + ' ' + password);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container py-5">
+    <!-- Props dengan kebab case -->
+    <!-- Bila props dengan bind gunakan (:) maka dianggap js syntax -->
+    <Person first-name="Eko" last-name="Khannedy" :age="30" />
+
+    <Alerts v-bind="alert" :show="true" :items="items" />
+  </div>
+</template>
---
+++ bekerja dengan komponen/src/components/Alerts.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <!-- V-for loop dengan integer -->
+        <!-- <Alert :message="message" type="danger" v-for="item in 3" :key="item" :show="true" /> -->
+
+        <Alert :message="message" v-for="(type, index) in types" :type="type" :key="index" :show="true" />
+    </section>
+</template>
+
+<script>
+import Alert from './Alert.vue';
+
+export default {
+    // props: ['message', 'types'],
+
+    // Props Validation
+    props: {
+        message: String,
+        types: Array,
+        items: Number
+    },
+    components: {
+        Alert
+    }
+}
+</script>
