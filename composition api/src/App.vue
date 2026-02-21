d318e38ec551fdd5b0dd54938b457f02532e87ff
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-21 19:27:54 +0700

    Pelepasan Event

--- composition api/src/App.vue
+++ composition api/src/App.vue
@@ -1,7 +1,11 @@
 <script>
 import { toRefs, reactive, ref, toRef, computed, watch, watchEffect } from 'vue';
+import CartItem from './components/CartItem.vue';
 
 export default {
+  components: {
+    CartItem
+  },
   setup() {
     const message = 'Hello Vue!';
 
@@ -37,12 +41,30 @@ export default {
 
     // Immediate membuat rekasi dimulai dari component dimuat
     // Secara default hanya bereaksi ketika property berubah saja
-    watch(total, () => console.log('total change from watch: ', total.value), {
-      immediate: true
-    })
+    // watch(total, () => console.log('total change from watch: ', total.value), {
+    //   immediate: true
+    // })
 
     // Secara default beraksi dimulai ketika compoent dimuat
-    watchEffect(() => console.log('total change from watchEffect:', total.value));
+    // watchEffect(() => console.log('total change from watchEffect:', total.value));
+
+    const items = reactive([
+      {
+        id: 1,
+        name: 'Abcde',
+        price: 12000
+      }
+    ]);
+
+    const handleRemove = (id) => {
+      let index = null;
+      items.forEach((data, key) => {
+        if (data.id === id) {
+          index = key;
+        }
+        items.splice(index, 1)
+      });
+    }
 
     return {
       message,
@@ -50,7 +72,9 @@ export default {
       decrement,
       product,
       swapProduct,
-      total
+      total,
+      items,
+      handleRemove
     }
   }
 }
@@ -58,16 +82,21 @@ export default {
 
 <template>
   <section>
-    <h1>{{ message }}</h1>
-    <button @click="increment">Increment</button>
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <button @click="increment">Increment</button>
     <button @click="decrement">Decrement</button>
 
     <p>{{ product.name }}</p>
     <p>{{ product.price }}</p>
-    <button @click="swapProduct">Swap</button>
+    <button @click="swapProduct">Swap</button> -->
 
-    Quantity : {{ product.quantity }}
-    Total : {{ total }}
+    <!-- Quantity : {{ product.quantity }}
+    Total : {{ total }} -->
+
+    <div v-for="(item, index) in items" :key="index">
+      <CartItem :item="item" @handle-remove="handleRemove" />
+    </div>
+
   </section>
 </template>
 
---
+++ composition api/src/components/CartItem.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+        }
+    },
+    emits: ['handleRemove'],
+    setup() {
+        return {
+            //
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <p>{{ item.name }}</p>
+        <p>{{ item.price }}</p>
+        <button @click="$emit('handleRemove', item.id)">Remove</button>
+    </section>
+</template>
