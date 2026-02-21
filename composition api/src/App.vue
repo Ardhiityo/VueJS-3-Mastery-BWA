917bf56d1bb325af8dad2d326819d6ff547c16f5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-21 14:47:28 +0700

    Composition API setup

---
+++ composition api/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  setup() {
+    const message = 'Hello Vue!';
+
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped></style>
