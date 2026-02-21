858c7c938ba5da237913461b22742c55b427d5bf
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-21 12:57:30 +0700

    Membuat Komponen Dasar Dropdown

---
+++ membuat komponen dropdown/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import Dropdown from './components/Dropdown.vue';
+
+export default {
+  components: {
+    Dropdown
+  }
+}
+</script>
+
+<template>
+  <div class="container menu-center">
+    <Dropdown>
+      <template #trigger>
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="true">
+          Dropdown button
+        </button>
+      </template>
+      <template #menu>
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </template>
+    </Dropdown>
+  </div>
+</template>
+
+<style scoped>
+.menu-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
---
+++ membuat komponen dropdown/src/components/Dropdown.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="dropdown show">
+        <slot name="trigger"></slot>
+        <ul class="dropdown-menu show"
+            style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(0px, 40px, 0px);"
+            data-popper-placement="bottom-start">
+            <slot name="menu"></slot>
+        </ul>
+    </div>
+</template>
