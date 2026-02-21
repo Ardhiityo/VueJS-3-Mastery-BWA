e20f9956d61211c5add7910bb37dcc3e7a97569f
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-21 16:58:14 +0700

    Bekerja dengan Properti Komputasi

--- composition api/src/App.vue
+++ composition api/src/App.vue
@@ -1,17 +1,14 @@
 <script>
-import { toRefs, reactive, ref, toRef } from 'vue';
+import { toRefs, reactive, ref, toRef, computed } from 'vue';
 
 export default {
   setup() {
     const message = 'Hello Vue!';
-    const quantity = ref(0);
-
-    const increment = () => quantity.value++;
-    const decrement = () => quantity.value--;
 
     const product = reactive({
       name: 'Product A',
-      price: 20000
+      price: 20000,
+      quantity: 0
     })
 
     const swapProduct = () => {
@@ -19,28 +16,32 @@ export default {
       product.price = 10000;
     }
 
+    const increment = () => product.quantity++;
+    const decrement = () => product.quantity--;
+    const total = computed(() => product.quantity * product.price);
+
     /**
      * toRef dan toRefs dipakai khusus untuk properti dalam object reactive, supaya tetap sinkron.
      */
-    const toref = toRef(product, 'name');
-    console.log(toref.value);
+    // const toref = toRef(product, 'name');
+    // console.log(toref.value);
 
-    product.name = 'Product B';
-    console.log('Change toref :', toref.value);
+    // product.name = 'Product B';
+    // console.log('Change toref :', toref.value);
 
-    const torefs = toRefs(product);
-    console.log(torefs.name.value);
+    // const torefs = toRefs(product);
+    // console.log(torefs.name.value);
 
-    product.name = 'Product C';
-    console.log('Change torefs :', torefs.name.value);
+    // product.name = 'Product C';
+    // console.log('Change torefs :', torefs.name.value);
 
     return {
       message,
-      quantity,
       increment,
       decrement,
       product,
-      swapProduct
+      swapProduct,
+      total
     }
   }
 }
@@ -49,13 +50,15 @@ export default {
 <template>
   <section>
     <h1>{{ message }}</h1>
-    <h1>{{ quantity }}</h1>
     <button @click="increment">Increment</button>
     <button @click="decrement">Decrement</button>
 
     <p>{{ product.name }}</p>
     <p>{{ product.price }}</p>
     <button @click="swapProduct">Swap</button>
+
+    Quantity : {{ product.quantity }}
+    Total : {{ total }}
   </section>
 </template>
 
