83e29168a0fcf870fe3d57452f6f88b9f4e495ba
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-21 14:57:06 +0700

    Data reaktif Menggunakan Fungsi Ref

--- composition api/src/App.vue
+++ composition api/src/App.vue
@@ -1,17 +1,31 @@
 <script>
+import { ref } from 'vue';
+
 export default {
   setup() {
     const message = 'Hello Vue!';
+    const quantity = ref(0);
+
+    const increment = () => quantity.value++;
+    const decrement = () => quantity.value--;
 
     return {
-      message
+      message,
+      quantity,
+      increment,
+      decrement
     }
   }
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <section>
+    <h1>{{ message }}</h1>
+    <h1>{{ quantity }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </section>
 </template>
 
 <style scoped></style>
