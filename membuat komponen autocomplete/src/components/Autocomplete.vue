bc6905bd3650993a6845595525d8996c2c6ae365
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-22 13:52:15 +0700

    Menambahkan fitur pencarian real time

--- membuat komponen autocomplete/src/components/Autocomplete.vue
+++ membuat komponen autocomplete/src/components/Autocomplete.vue
@@ -1,17 +1,38 @@
 <script setup>
+import { computed, ref } from 'vue';
+
 const { source } = defineProps({
     source: {
         type: Array,
         default: []
     }
 })
+
+const keyword = ref('');
+const keywordFound = ref(false);
+
+const result = computed(() => {
+    if (keywordFound.value) {
+        return [];
+    }
+    return source.filter(data => {
+        if (keyword.value.length) {
+            return data.name.toLowerCase().includes(keyword.value.toLowerCase());
+        }
+    })
+});
+
+const selectedKeyword = (name) => {
+    keyword.value = name
+    keywordFound.value = true;
+};
 </script>
 
 <template>
     <section>
-        <input type="text">
+        <input type="text" v-model="keyword">
         <ul>
-            <li v-for="(item, index) in source" :key="index">
+            <li v-for="(item, index) in result" :key="index" @click="selectedKeyword(item.name)">
                 {{ item.name }}
             </li>
         </ul>
