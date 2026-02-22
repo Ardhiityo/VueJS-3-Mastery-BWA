aec466cc113e6fc99219ff828dea4035e0ab2227
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-22 14:39:37 +0700

    mengimplementasikan v model pada komponen

--- membuat komponen autocomplete/src/App.vue
+++ membuat komponen autocomplete/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
+import { ref } from 'vue';
 import Autocomplete from './components/Autocomplete.vue';
 import source from "./source.json";
+
+const selectedKeyword = ref('');
+const handleSelectedKeyword = (name) => selectedKeyword.value = name; 
 </script>
 
 <template>
   <section>
-    Autocomplete
-    <Autocomplete :source="source" />
+    <h1>Selected : {{ selectedKeyword }}</h1>
+    <Autocomplete :source="source" @selected-keyword="handleSelectedKeyword" />
   </section>
 </template>
 
--- membuat komponen autocomplete/src/components/Autocomplete.vue
+++ membuat komponen autocomplete/src/components/Autocomplete.vue
@@ -1,15 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 const { source } = defineProps({
     source: {
         type: Array,
         default: []
+    },
+    modelValue: {
+        type: String
     }
 })
 
 const keyword = ref('');
+const emit = defineEmits(['selectedKeyword', 'update:modelValue']);
 const keywordFound = ref(false);
+const selectedKeyword = ref('');
 
 const result = computed(() => {
     if (keywordFound.value) {
@@ -22,17 +27,23 @@ const result = computed(() => {
     })
 });
 
-const selectedKeyword = (name) => {
+const handleSelectedKeyword = (name) => {
     keyword.value = name
     keywordFound.value = true;
+    selectedKeyword.value = name;
+    emit('selectedKeyword', name);
 };
+
+watch(keyword, (newValue, oldValue) => {
+    keywordFound.value && selectedKeyword.value != newValue ? keywordFound.value = false : '';
+});
 </script>
 
 <template>
     <section>
         <input type="text" v-model="keyword">
         <ul>
-            <li v-for="(item, index) in result" :key="index" @click="selectedKeyword(item.name)">
+            <li v-for="(item, index) in result" :key="index" @click="handleSelectedKeyword(item.name)">
                 {{ item.name }}
             </li>
         </ul>
