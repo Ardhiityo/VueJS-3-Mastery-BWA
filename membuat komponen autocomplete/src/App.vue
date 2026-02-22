0cbafa38ba6744c60e9436d7c30ec90aa6e34c64
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-22 12:57:59 +0700

    Membuat komponen dasar Autocomplete

---
+++ membuat komponen autocomplete/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Autocomplete from './components/Autocomplete.vue';
+import source from "./source.json";
+</script>
+
+<template>
+  <section>
+    Autocomplete
+    <Autocomplete :source="source" />
+  </section>
+</template>
+
+<style scoped></style>
---
+++ membuat komponen autocomplete/src/components/Autocomplete.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { source } = defineProps({
+    source: {
+        type: Array,
+        default: []
+    }
+})
+</script>
+
+<template>
+    <section>
+        <input type="text">
+        <ul>
+            <li v-for="(item, index) in source" :key="index">
+                {{ item.name }}
+            </li>
+        </ul>
+    </section>
+</template>
