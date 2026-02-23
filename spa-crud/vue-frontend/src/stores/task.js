b7c588b31406dd5ee7e31c1ce68bddc3709828b2
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-23 19:51:44 +0700

    Mendefinisikan Setup Store

--- spa-crud/vue-frontend/src/stores/task.js
+++ spa-crud/vue-frontend/src/stores/task.js
@@ -1,6 +1,9 @@
 import {defineStore} from "pinia";
 import {getTasks} from "../http/task-api";
+import {compile, computed, ref} from "vue";
 
+// Cara 1
+/*
 export const useTask = defineStore("useTask", {
   state: () => ({
     tasks: [],
@@ -15,14 +18,41 @@ export const useTask = defineStore("useTask", {
     // Cara 2
     completedTasks() {
       return this.tasks.filter((item) => item.is_completed);
-    },
-  },
-  actions: {
-    fetchTasks: async function () {
-      const response = await getTasks();
-      if (response.status === 200) {
-        this.tasks = response.data.data;
-      }
-    },
-  },
+      },
+      },
+      actions: {
+        fetchTasks: async function () {
+          const response = await getTasks();
+          if (response.status === 200) {
+            this.tasks = response.data.data;
+            }
+            },
+            },
+            });
+            */
+
+export const useTask = defineStore("useTask", () => {
+  const tasks = ref([]);
+
+  const uncompletedTasks = computed(() =>
+    tasks.value.filter((item) => !item.is_completed),
+  );
+
+  const completedTasks = computed(() =>
+    tasks.value.filter((item) => item.is_completed),
+  );
+
+  const fetchTasks = async () => {
+    const response = await getTasks();
+    if (response.status === 200) {
+      tasks.value = response.data.data;
+    }
+  };
+
+  return {
+    tasks,
+    uncompletedTasks,
+    completedTasks,
+    fetchTasks,
+  };
 });
