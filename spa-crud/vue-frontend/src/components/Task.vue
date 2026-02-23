2ae9156099b8873740a0c8bfc66d142fccbb34e7
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-23 13:23:58 +0700

    Membuat fitur Inline Editing

--- spa-crud/vue-frontend/src/components/Task.vue
+++ spa-crud/vue-frontend/src/components/Task.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { defineProps, computed } from 'vue';
-import PencilIcon from './icons/PencilIcon.vue';
-import TrashIcon from './icons/TrashIcon.vue'
+import { defineProps, computed, ref } from 'vue';
+import TaskAction from './TaskAction.vue';
 
 const { task } = defineProps({
     task: {
@@ -10,6 +9,12 @@ const { task } = defineProps({
 })
 
 const isCompletedTask = computed(() => task.is_completed ? true : false);
+
+const isEdit = ref(false);
+
+const vFocus = {
+    mounted: (el) => el.focus()
+}
 </script>
 
 <template>
@@ -17,16 +22,14 @@ const isCompletedTask = computed(() => task.is_completed ? true : false);
         <div class="d-flex justify-content-start align-items-center">
             <input class="form-check-input mt-0" type="checkbox" :checked="isCompletedTask" />
             <div class="ms-2 flex-grow-1" :class="{ completed: isCompletedTask }"
-                title="Double click the text to edit or remove">
-                <div class="relative">
+                title="Double click the text to edit or remove" @dblclick="isEdit = !isEdit">
+                <div class="relative" v-if="isEdit">
+                    <input class="editable-task" type="text" @keyup.esc="isEdit = !isEdit" v-focus />
                 </div>
-                <span>{{ task.name }}</span>
+                <span v-else>{{ task.name }}</span>
             </div>
             <div class="task-date">24 Feb 12:00</div>
         </div>
-        <div class="task-actions">
-            <PencilIcon />
-            <TrashIcon />
-        </div>
+        <TaskAction @edit="isEdit = !isEdit" v-if="!isEdit" />
     </li>
 </template>
---
+++ spa-crud/vue-frontend/src/components/TaskAction.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PencilIcon from './icons/PencilIcon.vue';
+import TrashIcon from './icons/TrashIcon.vue'
+
+import { defineEmits } from 'vue';
+
+defineEmits(['edit']);
+</script>
+
+<template>
+    <div class="task-actions">
+        <PencilIcon @click="$emit('edit')" />
+        <TrashIcon />
+    </div>
+</template>
