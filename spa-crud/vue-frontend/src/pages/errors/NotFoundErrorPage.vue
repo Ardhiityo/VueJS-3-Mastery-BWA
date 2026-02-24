aeaaa2e16b759b2aeed0cd006199190e4c8f96f8
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-24 13:13:07 +0700

    Penanganan error 404

---
+++ spa-crud/vue-frontend/src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        <h1>Not Found</h1>
+        <!-- <p>Sorry, the page you're looking for {{ $route.fullPath }} its not found!</p> -->
+        <!-- result : /sdkjfbdjfi -->
+
+        <p>Sorry, the page you're looking for {{ $route.params.pathMatch }} its not found!</p>
+        <!-- result : sdkjfbdjf -->
+    </section>
+</template>
--- spa-crud/vue-frontend/src/router/routes.js
+++ spa-crud/vue-frontend/src/router/routes.js
@@ -2,6 +2,7 @@ import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import TaskPage from "../pages/TaskPage.vue";
 import SummaryPage from "../pages/SummaryPage.vue";
+import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue";
 
 const routes = [
   {
@@ -23,6 +24,11 @@ const routes = [
     path: "/summaries",
     name: "summary",
     component: SummaryPage,
+  },
+  {
+    path: "/:pathMatch",
+    name: "error.404",
+    component: NotFoundErrorPage,
   },
 ];
 
